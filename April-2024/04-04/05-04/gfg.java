eac7bbf17357f25717e712ade0efce1770766078
Swayam Pendurkar <109409328+Swayam1812@users.noreply.github.com>
2024-04-04 07:02:20 +0530

    Create gfg.java

---
+++ April-2024/04-04/05-04/gfg.java
@@ -0,0 +1,17 @@
+class Solution
+{
+    //Function to find sum of all possible substrings of the given string.
+    
+     public static long MOD = 1000000007;
+    public static long sumSubstrings(String s) {
+        long n = s.length();
+        long sum = 0;
+        long multiplier = 1;  
+        for (int i = (int) n - 1; i >= 0; i--) {
+            long digit = s.charAt(i) - '0';
+            sum = (sum + (digit * multiplier * (i + 1)) % MOD) % MOD;
+            multiplier = (multiplier * 10 + 1) % MOD;
+        }
+        return sum;
+    }
+}
