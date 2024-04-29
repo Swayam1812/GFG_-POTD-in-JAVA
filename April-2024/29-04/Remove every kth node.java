25776004c016246986decacec67e1d8c2e8bde94
Swayam Pendurkar <109409328+Swayam1812@users.noreply.github.com>
2024-04-29 20:36:34 +0530

    Remove every kth node.java

---
+++ April-2024/29-04/Remove every kth node.java
@@ -0,0 +1,21 @@
+class Solution
+{
+    /*You are required to complete this method*/
+    Node delete(Node head, int k)
+    {
+// Your code here 
+if(k==1) return null;
+int count = 1;
+Node temp = head;
+while(temp.next != null && temp.next.next !=null){
+    if(count == (k-1)){
+        temp.next = temp.next.next;
+        count=1;
+    }else 
+        count++;
+    temp = temp.next;
+}
+if(count == (k-1)) temp.next = null;
+return head;
+    }
+}
