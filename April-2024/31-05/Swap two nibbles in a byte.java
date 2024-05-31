ea05f23007100a6381c5672d9190dcd9dc99efeb
Swayam Pendurkar <109409328+Swayam1812@users.noreply.github.com>
2024-05-31 23:42:57 +0530

    Swap two nibbles in a byte.java

---
+++ April-2024/31-05/Swap two nibbles in a byte.java
@@ -0,0 +1,7 @@
+class Solution {
+    static int swapNibbles(int n) {
+        int first=n%16;  //FIRST NIBBLE
+        int second=n/16; //SECOND NIBBLE
+        return first*16+second;  //COMBINING TWO NIBBLES
+    }
+}
