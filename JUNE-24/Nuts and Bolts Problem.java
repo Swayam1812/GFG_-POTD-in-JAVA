d69df16b5c8808c5d78656772b4ecf9433298c44
Swayam Pendurkar <109409328+Swayam1812@users.noreply.github.com>
2024-06-10 21:49:39 +0530

    Nuts and Bolts Problem.java

---
+++ JUNE-24/Nuts and Bolts Problem.java
@@ -0,0 +1,9 @@
+class Solution {
+    void matchPairs(int n, char nuts[], char bolts[]) {
+        // The Arrays.sort method is used to sort the nuts array in ascending order.
+        Arrays.sort(nuts);
+
+        // The Arrays.sort method is used to sort the bolts array in ascending order.
+        Arrays.sort(bolts);
+    }
+}
